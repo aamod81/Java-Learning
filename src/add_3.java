77c8b22ccde215185cf51292ccb33b75c364d4f0
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2025-09-19 22:30:10 +0530

    add3

---
+++ src/add_3.java
@@ -0,0 +1,9 @@
+public class add_3 {
+    public static void main(String[] args){
+        int num1=5;
+        int num2=10;
+        int num3=11;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+}
