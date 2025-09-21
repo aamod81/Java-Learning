132ea4184833d7c362b8775170b04c01a2fc638d
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2025-09-21 10:50:17 +0530

    grade encrypt decrypt

---
+++ src/exe2gradeencrypt.java
@@ -0,0 +1,13 @@
+public class exe2gradeencrypt {
+    public static void main(String[] args) {
+        char grade= 'f';
+        grade = (char)(grade+2);
+        System.out.println(grade);
+        grade = (char)(grade-2);
+        System.out.println(grade);
+
+
+
+
+    }
+}
