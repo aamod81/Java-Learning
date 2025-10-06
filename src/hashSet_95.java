e706fb4fff81decb2a3ecc264f8c6d2c401d2ebb
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2025-10-06 23:48:08 +0530

    hashset

---
+++ src/hashSet_95.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class hashSet_95 {
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<Integer>(6,8);
+        hs.add(6);
+        hs.add(5);
+        hs.add(7);
+        System.out.println(hs);
+    }
+}
