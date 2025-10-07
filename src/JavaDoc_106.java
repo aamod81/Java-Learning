26d9722e324ca316e32c5326623f0a9e6210916d
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2025-10-07 15:15:03 +0530

    javadoc

---
+++ src/JavaDoc_106.java
@@ -0,0 +1,24 @@
+/**
+ * This class is to demonstrate what javadoc is and how it's used in java industry
+ * @author Aamod
+ * @version 0.1
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/">Java 8 API Documentation> java docs</a>
+ */
+public class JavaDoc_106 {
+    /**
+     * Adds two integers and prints their sum.
+     * @param a first integer
+     * @param b second integer
+     */
+    public void add(int a, int b){
+        System.out.println("The sum is: " + (a + b));
+    }
+
+    /**
+     * The main method – program entry point.
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
+}
