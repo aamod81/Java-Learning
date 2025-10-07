ae94b3110af6ef3557c573d262a1951bb80d133a
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2025-10-07 14:15:35 +0530

    dateformatter

---
+++ src/LocalDate_101.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDate_101 {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy||MM||dd ");
+        DateTimeFormatter dtf2 = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        String str = dt.format(dtf);
+        System.out.println(str);
+    }
+}
