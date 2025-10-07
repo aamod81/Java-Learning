e424e4fe86a1c689f0c011cc3ca54b4d7c83ba9b
Aamod Kumar Dwivedi <amodkumardwivedi0081@gmail.com>
2025-10-08 00:19:32 +0530

    lambda

--- src/lambda_109.java
+++ src/lambda_109.java
@@ -1,37 +1,46 @@
 public class lambda_109 {
     interface DemoAno {
         void meth1();
-
         void meth2();
     }
 
-    class AnonyDemo implements DemoAno {
+    static final class AnonyDemo implements DemoAno {
         public void display() {
             System.out.println("This is my display method");
         }
 
         @Override
         public void meth1() {
-            System.out.println("This is my meth1 method");
+            System.out.println("This is my meth1 method from AnonyDemo");
         }
 
         @Override
         public void meth2() {
-            System.out.println("This is my meth2 method");
+            System.out.println("This is my meth2 method from AnonyDemo");
         }
+    }
 
-        public static void main(String[] args) {
-            DemoAno obj = new DemoAno() {
-                @Override
-                public void meth1() {
-                    System.out.println("This is my meth1 method");
-                }
+    public static void main(String[] args) {
+        // Using normal class implementation
+        AnonyDemo obj1 = new AnonyDemo();
+        obj1.display();
+        obj1.meth1();
+        obj1.meth2();
 
-                @Override
-                public void meth2() {
-                    System.out.println("This is my meth2 method");
-                }
-            };
-        }
+        // Using anonymous class
+        DemoAno obj2 = new DemoAno() {
+            @Override
+            public void meth1() {
+                System.out.println("This is my meth1 method from anonymous class");
+            }
+
+            @Override
+            public void meth2() {
+                System.out.println("This is my meth2 method from anonymous class");
+            }
+        };
+
+        obj2.meth1();
+        obj2.meth2();
     }
 }
